f5c829db31938d45cd93c71b7337ececac7bc47f
Chathushi Jayarathna <chathushi0707@gmail.com>
2023-06-28 00:21:51 +0530

    WFS

---
+++ Module 8 - WFS/Project - AAA Hosting/Source Code/AAAGroups/modules/AAACustomerPortlet/src/main/java/com/lithna/aaa/constants/AAACustomerPortletKeys.java
@@ -0,0 +1,11 @@
+package com.lithna.aaa.constants;
+
+/**
+ * @author PC
+ */
+public class AAACustomerPortletKeys {
+
+	public static final String AAACUSTOMER =
+		"com_lithna_aaa_AAACustomerPortlet";
+
+}
